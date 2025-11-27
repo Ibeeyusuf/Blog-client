9dea1ec66d83b511c75603360aec25428b924c82
Ibeeyusuf <ibroyusuf144@gmail.com>
2025-11-27 18:05:35 +0100

    new changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://localhost:5000/api'; // Adjust to your backend URL
+// export const API_BASE_URL = 'http://localhost:5000/api'; // Adjust to your backend URL
+export const API_BASE_URL = 'https://blog-server-yime.onrender.com/api';
